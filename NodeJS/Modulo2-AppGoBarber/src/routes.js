67d51e32554b9d24c3e0db4ed17812fd36b47753
Leonardo Leite <leonardotleite13k@gmail.com>
2019-02-23 15:02:56 -0300

    Iniciando o tratamento com Middlewares

--- NodeJS/Modulo2-AppGoBarber/src/routes.js
+++ NodeJS/Modulo2-AppGoBarber/src/routes.js
@@ -3,8 +3,16 @@ const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  res.render('dashboard')
+})
 module.exports = routes
