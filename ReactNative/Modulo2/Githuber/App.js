e9ca80ea53daaa427b6d44bfd2324774452c3924
Leonardo Leite <leonardotleite13k@gmail.com>
2019-03-28 00:25:28 -0300

    Iniciando Modulo 2

---
+++ ReactNative/Modulo2/Githuber/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import './src/config/ReactotronConfig';
+import './src/config/DevToolsConfig';
+
+export default class App extends Component {
+  singIn = () => {};
+
+  render() {
+    return (
+      <View>
+        <Text>Teste</Text>
+      </View>
+    );
+  }
+}
