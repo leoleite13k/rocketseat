3a8764604510ccd3772b5a5b00bd192aa34f45a2
Leonardo Leite <leonardotleite13k@gmail.com>
2019-03-31 13:55:38 -0300

    Iniciando Modulo 3

---
+++ ReactNative/Modulo3/Redux/src/index.js
@@ -0,0 +1,17 @@
+import '~/config/ReactotronConfig';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import TodoList from './TodoList';
+
+// import styles from './styles';
+
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+
+export default App;
