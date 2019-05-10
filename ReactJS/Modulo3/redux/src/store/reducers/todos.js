5ea4958cf36d81f9cee0ca095efd24e023ef3cc7
Leonardo Leite <leonardotleite13k@gmail.com>
2019-05-10 13:22:24 -0300

    Iniciando modulo 3

---
+++ ReactJS/Modulo3/redux/src/store/reducers/todos.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = [
+  { id: 1, text: "Fazer café" },
+  { id: 2, text: "Estudar React" }
+];
+
+export default function todos(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      return state;
+  }
+}
