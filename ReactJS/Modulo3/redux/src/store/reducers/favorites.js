cdfb0a72ddfbb8ac441a19ed3748d31e61b50e88
Leonardo Leite <leonardotleite13k@gmail.com>
2019-06-12 07:36:43 -0300

    Utilizando Redux Saga e tratando error e loading

--- ReactJS/Modulo3/redux/src/store/reducers/favorites.js
+++ ReactJS/Modulo3/redux/src/store/reducers/favorites.js
@@ -1,17 +1,19 @@
-const INITIAL_STATE = [];
+const INITIAL_STATE = {
+  data: [],
+  loading: false,
+  error: null,
+};
 
 export default function favorites(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'ADD_FAVORITE':
-      return [
-        ...state,
-        {
-          id: Math.random(),
-          name: 'facebook/react',
-          description: 'React is a JavaScript library for building user interfaces',
-          url: 'https://github.com/facebook/react',
-        },
-      ];
+    case 'ADD_FAVORITE_REQUEST':
+      return { ...state, loading: true };
+    case 'ADD_FAVORITE_SUCCESS':
+      return {
+        ...state, loading: false, error: null, data: [...state.data, action.payload.data],
+      };
+    case 'ADD_FAVORITE_FAILURE':
+      return { ...state, loading: false, error: action.payload.error };
     default:
       return state;
   }
