a827cf046ee979bf8b182e470a42ac11ca8871a7
Leonardo Leite <leonardotleite13k@gmail.com>
2019-07-04 13:26:38 -0300

    feature: Iniciando novo servidor
    modulo: 1

--- NodeJS/modulo01/index.js
+++ NodeJS/modulo01/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", () => {
-  console.log("teste");
+const users = ["Carlos", "Marcelo", "Thiago"];
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
 });
 
 server.listen(3000);
