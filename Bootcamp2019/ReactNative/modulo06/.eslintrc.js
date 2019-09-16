4f0cf292ea708782f3daa07729e1c074168ea337
Leonardo Leite <leonardotleite13k@gmail.com>
2019-09-16 05:52:17 -0300

    feature: Finalizado modulo06

--- Bootcamp2019/ReactNative/modulo06/.eslintrc.js
+++ Bootcamp2019/ReactNative/modulo06/.eslintrc.js
@@ -2,14 +2,11 @@ module.exports = {
   env: {
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
@@ -19,15 +16,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
-      'warn', {
-        extensions: ['.jsx', '.js']
-      }
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
     ],
     'import/prefer-default-export': 'off',
   },
