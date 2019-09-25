e803dec8b5b0a060a1a0ea68b0be3e617eaae475
Leonardo Leite <leonardotleite13k@gmail.com>
2019-09-25 17:49:58 -0300

    Api d eprodutos

---
+++ Bootcamp2019/NodeJS/node-api/server.js
@@ -0,0 +1,22 @@
+express = require("express");
+cors = require("cors");
+mongoose = require("mongoose");
+requireDir = require("require-dir");
+
+// Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+requireDir("./src/models");
+
+// Rotas
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
