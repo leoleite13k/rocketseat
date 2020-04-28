437ca2fb8873fa281a32f3513aec8b818773ccc6
Leonardo Leite <leonardotleite13k@gmail.com>
2020-04-28 18:41:02 -0300

    feature: Finished level01 TypeScript

--- Bootcamp2020/TypeScript/level01/src/routes.ts
+++ Bootcamp2020/TypeScript/level01/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
-  return response.json({ message: 'Hello World' });
+  const user = createUser({
+    email: 'leonardotleite13k@gmail.com',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'Native', experience: 90 },
+    ],
+  });
+
+  return response.json(user);
 }
