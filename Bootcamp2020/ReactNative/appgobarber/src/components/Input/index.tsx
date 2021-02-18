f5724238bd7c710939f113c753c9d668e7f4a91c
Leonardo Leite <leonardotleite13k@gmail.com>
2021-02-18 20:49:00 -0300

    Inserted unform lib

--- Bootcamp2020/ReactNative/appgobarber/src/components/Input/index.tsx
+++ Bootcamp2020/ReactNative/appgobarber/src/components/Input/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,7 +11,32 @@ interface IInput extends TextInputProps {
   icon: string;
 }
 
+interface IInputValueRef {
+  value: string;
+}
+
 const Input: React.FC<IInput> = ({ name, icon, ...rest }) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<IInputValueRef>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       <Icon name={icon} size={20} color="#666360" />
@@ -16,6 +44,10 @@ const Input: React.FC<IInput> = ({ name, icon, ...rest }) => {
       <TextInput
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
         {...rest}
       />
     </Container>
