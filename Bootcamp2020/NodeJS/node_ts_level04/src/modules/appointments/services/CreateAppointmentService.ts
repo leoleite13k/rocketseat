5f6e09b74ebb68a3e26e403048d84b946a020d3c
Leonardo Leite <leonardotleite13k@gmail.com>
2021-03-17 08:50:11 -0300

    Inject dependecy with tsyringe

--- Bootcamp2020/NodeJS/node_ts_level04/src/modules/appointments/services/CreateAppointmentService.ts
+++ Bootcamp2020/NodeJS/node_ts_level04/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,9 @@
 import { startOfHour } from 'date-fns';
+import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
-import { IAppointmentsRepository } from '../repositories/IAppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 // SOLID
 // Dependency Inversion
@@ -12,8 +13,12 @@ interface IRequest {
   date: Date;
 }
 
+@injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
   public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
