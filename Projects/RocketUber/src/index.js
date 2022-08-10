0c625a7d75a7285c00d6549ea069b8477940dd40
Leonardo Leite <leonardotleite13k@gmail.com>
2022-08-10 11:52:44 -0300

    refactor: organizing folders

--- Projects/RocketUber/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-import Map from './components/Map';
-
-export default function src() {
-  return <Map />;
-}
--- Workshops/fluidTransition/README.md
+++
@@ -1,9 +0,0 @@
-# Workshop TRansições com React Native
-
-## Setup
-Inicie um novo projeto
-
-```bash
-yarn install
-yarn start
-```
